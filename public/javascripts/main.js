ab16765126310454e01ccefcdb84b2b85a8abb43
CesarRosales16 <00060917@uca.edu.sv>
2018-12-01 15:55:56 -0600

    se añadio routa api

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,11 @@
 tareas();
 console.log(document.forms.formRegister.modelo.value);
+document.querySelector("#formRegister").addEventListener('submit',function(e) {
+    e.preventDefault();
+    let data = {
+        modelo: document.forms.formRegister.modelo.value,
+        marca: document.forms.formRegister.marca.value,
+        placa: document.forms.formRegister.placa.value,
+    }
+    fetch('api/Automoviles')
+})
---
+++ routes/api.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const automovilController = require('../controllers/automovilController'); 
+
+/* GET users listing. */
+router.get('/Automoviles', automovilController.index);
+router.get('/Automoviles/:id', automovilController.findAutomovil);
+router.post('/Automoviles/',automovilController.store);
+router.put('/Automoviles/:id',automovilController.update);
+router.delete('/Automoviles/:id',automovilController.delete);
+module.exports = router;
